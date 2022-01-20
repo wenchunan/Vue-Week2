1206a95869dc9bc8ecf0f2c470e62ed334b9e4e6
wenchunan <wenchunan18@gmail.com>
2022-01-20 12:54:07 +0800

    商品頁修改

--- js/products.js
+++ js/products.js
@@ -3,48 +3,55 @@ import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue
 createApp({
   data() {
     return {
-      apiUrl: "https://vue3-course-api.hexschool.io/v2",
-      apiPath: "annawen",
+      apiUrl: "https://vue3-course-api.hexschool.io/v2", // 請加入站點
+      apiPath: "annawen", // 請加入個人 API Path
       products: [],
       temp: {},
     };
   },
   methods: {
-    checkLogin() {
+    checkAdmin() {
       const url = `${this.apiUrl}/api/user/check`;
       axios
         .post(url)
-        .then((res) => {
-          //   console.log(res.data);
+        .then(() => {
           this.getProducts();
         })
         .catch((err) => {
-          console.dir(err);
+          alert(err.data.message);
           window.location = "index.html";
         });
     },
     getProducts() {
-      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+      // #5 取得後台產品列表
       axios
-        .get(url)
+        .get(`${this.apiUrl}/api/${this.apiPath}/admin/products`)
         .then((res) => {
           console.log(res.data);
           this.products = res.data.products;
         })
-        .catch((err) => {
-          console.dir(err);
+        .catch((error) => {
+          console.dir(error);
+          alert(error.data.message);
         });
     },
     openProduct(item) {
       this.temp = item;
     },
   },
+
+  //類似init();
   created() {
+    // 取出 Token
     const token = document.cookie.replace(
       /(?:(?:^|.*;\s*)annaToken\s*\=\s*([^;]*).*$)|^.*$/,
       "$1"
     );
+
+    //token存到Authorization
+    //在每一次打API時，會預設帶入token
     axios.defaults.headers.common["Authorization"] = token;
-    this.checkLogin();
+
+    this.checkAdmin();
   },
 }).mount("#app");
